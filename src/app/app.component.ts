f46beef4cead13288e640c5768d02b9c05a27bbd
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-05-22 22:06:11 +0530

    complete navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavBarComponent} from "./nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    imports: [RouterOutlet, NavBarComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'dilanka-rathnasiri-portfolio';
