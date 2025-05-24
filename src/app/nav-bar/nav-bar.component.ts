8ab239f9d75f62f6dbf401f390b3d26a87431be0
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-05-24 23:27:46 +0530

    complete footer

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { SocialMediaComponent } from '../social-media/social-media.component';
 
 @Component({
     selector: 'app-nav-bar',
-    imports: [RouterLink, RouterLinkActive],
+    imports: [RouterLink, RouterLinkActive, SocialMediaComponent],
     templateUrl: './nav-bar.component.html',
     styleUrls: ['./nav-bar.component.scss'],
 })
