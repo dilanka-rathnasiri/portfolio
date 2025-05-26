828540088c661bbb5be7b7753d172b9770f940b8
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-05-26 23:03:31 +0530

    Complete experience

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -4,6 +4,5 @@ import { Component } from '@angular/core';
     selector: 'app-experience',
     imports: [],
     templateUrl: './experience.component.html',
-    styleUrl: './experience.component.scss',
 })
 export class ExperienceComponent {}
