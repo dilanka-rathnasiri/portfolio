94a980f3c9ab6312c120f5527cf2288523b9dde0
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-06-13 20:29:09 +0530

    fix css in what I do box

---
+++ src/app/dto/WhatIDoItems.ts
@@ -0,0 +1,5 @@
+export interface WhatIDoItems {
+    title: string;
+    icon: string;
+    description: string;
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WhatIDoItems } from '../dto/WhatIDoItems';
 
 @Component({
     selector: 'app-home',
@@ -6,4 +7,28 @@ import { Component } from '@angular/core';
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+    whatIDoItems: WhatIDoItems[] = [
+        {
+            title: 'Backend Development',
+            icon: 'fa-solid fa-server',
+            description:
+                'Building robust and maintainable applications using Java,' +
+                ' Python, and modern frameworks',
+        },
+        {
+            title: 'Cloud Engineering',
+            icon: 'fa-solid fa-cloud',
+            description:
+                'Designing and implementing scalable cloud solutions using' +
+                ' AWS services and best practices',
+        },
+        {
+            title: 'DevOps',
+            icon: 'fa-solid fa-gears',
+            description:
+                'Implementing CI/CD pipelines and infrastructure as code' +
+                ' using Terraform and GitLab CI/CD',
+        },
+    ];
+}
