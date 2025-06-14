2a6c903b4ae17e681995c1b8409e9ab4a223b820
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-06-14 10:54:37 +0530

    fix css in home skills box

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WhatIDoItems } from '../dto/WhatIDoItems';
+import { SkillItem } from '../dto/SkillItem';
 
 @Component({
     selector: 'app-home',
@@ -30,5 +31,17 @@ export class HomeComponent {
                 'Implementing CI/CD pipelines and infrastructure as code' +
                 ' using Terraform and GitLab CI/CD',
         },
+    ];
+
+    skillsItems: SkillItem[] = [
+        { name: 'Java', icon: 'fa-brands fa-java' },
+        { name: 'Python', icon: 'fa-brands fa-python' },
+        { name: 'AWS', icon: 'fa-brands fa-aws' },
+        { name: 'Spring Boot', icon: 'fa-brands fa-java' },
+        { name: 'REST API', icon: 'fa-solid fa-cloud' },
+        { name: 'GitLab CI/CD', icon: 'fa-brands fa-gitlab' },
+        { name: 'Terraform', icon: 'fa-brands fa-dev' },
+        { name: 'FastAPI', icon: 'fa-brands fa-python' },
+        { name: 'Kubernetes', icon: 'fa-brands fa-docker' },
     ];
 }
