07d0393e53520c695c7a4f576b063e83c730672e
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-06-15 14:14:33 +0530

    complete blogs page

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BlogItem } from '../dto/BlogItem';
 
 @Component({
     selector: 'app-blog',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
     templateUrl: './blog.component.html',
     styleUrl: './blog.component.scss',
 })
-export class BlogComponent {}
+export class BlogComponent {
+    blogs: BlogItem[] = [
+        {
+            title: 'Understanding Angular Signals',
+            description:
+                'A deep dive into Angular signals and how they improve reactivity in modern applications.',
+            link: 'https://blog.example.com/angular-signals',
+        },
+        {
+            title: 'Deploying with AWS Amplify',
+            description:
+                'Step-by-step guide to deploying your web app using AWS Amplify.',
+            link: 'https://blog.example.com/aws-amplify-deploy',
+        },
+        {
+            title: 'TypeScript Tips & Tricks',
+            description:
+                'Boost your productivity with these essential TypeScript tips.',
+            link: 'https://blog.example.com/typescript-tips',
+        },
+    ];
+}
