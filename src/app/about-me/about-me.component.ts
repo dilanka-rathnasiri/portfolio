f2cc3d373e71e0785d622d2f5bff4eb3128f88ee
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-06-22 13:30:18 +0530

    optimize image

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
     selector: 'app-about-me',
-    imports: [],
+    imports: [NgOptimizedImage],
     templateUrl: './about-me.component.html',
     styleUrl: './about-me.component.scss',
 })
