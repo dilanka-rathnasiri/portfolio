37c1b50f9b6c6ba4f53e7e833347e0187728c94b
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-08-16 23:03:30 +0530

    make navbar responsive

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SocialMediaComponent } from '../social-media/social-media.component';
 import { NavBarItem } from '../dto/NavBarItem';
@@ -10,9 +10,18 @@ import { NavBarItem } from '../dto/NavBarItem';
     styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
+    isMenuCollapsed = true;
     menuItems: NavBarItem[] = [
         { label: 'Home', link: '/', exact: true },
         { label: 'Projects', link: '/projects', exact: false },
         { label: 'Blogs', link: '/blogs', exact: false },
     ];
+
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+        // Close menu when screen size changes to desktop
+        if (window.innerWidth >= 992) { // 992px is Bootstrap's lg breakpoint
+            this.isMenuCollapsed = true;
+        }
+    }
 }
