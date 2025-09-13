f751c67abcd097350d72443b707f5dbd8b6b38ab
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-09-13 12:57:22 +0530

    convert what I do section to primeng

--- src/app/what-i-do/what-i-do.component.ts
+++ src/app/what-i-do/what-i-do.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
 import { WhatIDoItem } from '../dto/WhatIDoItem';
 import { DataService } from '../services/data.service';
 
 @Component({
     selector: 'app-what-i-do',
-    imports: [],
+    imports: [CommonModule, CardModule],
     templateUrl: './what-i-do.component.html',
+    styleUrl: './what-i-do.component.scss',
 })
 export class WhatIDoComponent implements OnInit {
     whatIDoItems: WhatIDoItem[] = [];
