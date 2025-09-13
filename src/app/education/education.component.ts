9e55d971a9753bc4fd8cae2669f0a57135110e9d
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-09-13 13:35:13 +0530

    convert education section to primeng

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
 import { DataService } from '../services/data.service';
 import { EducationItem } from '../dto/EducationItem';
 
 @Component({
     selector: 'app-education',
-    imports: [],
+    imports: [CommonModule, CardModule],
     templateUrl: './education.component.html',
 })
 export class EducationComponent implements OnInit {
