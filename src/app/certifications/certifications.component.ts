bc5768bfa978e7898f92dd0e065115a632ab1c31
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-09-13 13:40:44 +0530

    convert complete home to primeng

--- src/app/certifications/certifications.component.ts
+++ src/app/certifications/certifications.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 import { CertificateItem } from '../dto/CertificateItem';
 import { DataService } from '../services/data.service';
+import { LinkService } from '../services/link.service';
 
 @Component({
     selector: 'app-certifications',
-    imports: [],
+    imports: [CommonModule, CardModule, ButtonModule],
     templateUrl: './certifications.component.html',
 })
 export class CertificationsComponent implements OnInit {
     certificates: CertificateItem[] = [];
 
-    constructor(private dataService: DataService) {}
+    constructor(
+        private dataService: DataService,
+        private linkService: LinkService
+    ) {}
 
     ngOnInit(): void {
         this.dataService
@@ -18,5 +25,9 @@ export class CertificationsComponent implements OnInit {
             .subscribe((data) => {
                 this.certificates = data;
             });
+    }
+
+    openCertificateLink(url: string): void {
+        this.linkService.openLinkInNewTab(url);
     }
 }
